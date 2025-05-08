ac5a08cf69e5402e72149894e9b9b68962ae634d
Felicio <feliciorar@gmail.com>
2025-05-08 10:52:45 -0300

    fix uuids

--- app/Models/Movement.php
+++ app/Models/Movement.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movement extends Model
 {
     /** @use HasFactory<\Database\Factories\MovementsFactory> */
-    use HasFactory, HasUlids;
+    use HasFactory, HasUuids;
 
     protected $keyType = 'string';
     public $incrementing = false;
